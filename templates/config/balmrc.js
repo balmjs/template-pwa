7fdcbfe173b38e3817ae0543d6af60ca93fc2b35
elf-mouse <ifmouz@gmail.com>
2018-01-25 13:59:51 +0800

    update balm@0.16.1

--- templates/config/balmrc.js
+++ templates/config/balmrc.js
@@ -30,5 +30,6 @@ module.exports = {
     excludes: ['service-worker.js']
   }
   // More Config
-  // https://github.com/balmjs/balm/blob/master/docs/configuration.md
+  // Documentation - http://balmjs.com/docs/en/configuration/toc.html
+  // 中文文档 - http://balmjs.com/docs/zh-cn/configuration/toc.html
 };
